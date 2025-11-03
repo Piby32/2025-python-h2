cb526b4466cdf63bb9dfa09c76e3e27b773e2995
Piby32 <piby1216@gmail.com>
2025-11-03 20:29:00 +0800

    Create ex3-1.py

---
+++ ex3-1.py
@@ -0,0 +1,16 @@
+#字串(string)
+name = "piby"
+message = "welcome to Python"
+print(name)
+print(type(name))       # <class 'str'>
+#整數(instger)
+age = 10
+print(type(age))        # <class 'int'>
+#浮點數(float)
+score = 490.5
+print(type(score))     # <class 'float'>
+#布林值(boolean)
+is_student = True
+has_license = False
+print(type(is_student))  # <class 'bool'>
+print(type(has_license)) # <class 'bool'>
