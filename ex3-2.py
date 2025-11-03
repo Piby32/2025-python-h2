161bad7d726aabd282a0c0559386f5f0c67b5683
Piby32 <piby1216@gmail.com>
2025-11-03 20:40:16 +0800

    Create ex3-2.py

---
+++ ex3-2.py
@@ -0,0 +1,16 @@
+#字串(string)
+name = input("name: ")
+message = "welcome to Python"
+print("name: ",name)
+print(message)
+#整數(instger)
+age = input("age: ")
+print("age: ",age)
+#浮點數(float)
+score = input("score: ")
+print("score: ",score)
+#布林值(boolean)
+is_student = True
+has_license = False
+print(type(is_student))  # <class 'bool'>
+print(type(has_license)) # <class 'bool'>
