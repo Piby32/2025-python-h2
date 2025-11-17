735230bd220945b06612a72db8714c14fe9da76b
Piby32 <piby1216@gmail.com>
2025-11-17 20:58:56 +0800

    Create ex4-1.py

---
+++ ex4-1.py
@@ -0,0 +1,39 @@
+print(True and False)
+print(True or False)
+print(not True)
+
+
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
+x = 15
+y = 4
+print("this is a book\n\t","hello world")
+
+
+age = int(input("age"))
+if age >= 18:
+    print("您已成年，可以投票!")
+else:
+    print("您未成年，無法投票。")
+
+
+age = int(input("score"))
+if age >= 90:
+    print("A")
+elif age >= 80:
+    print("B")
+elif age >= 70:
+    print("C")
+elif age >= 60:
+    print("D")
+else:
+    print("E")
